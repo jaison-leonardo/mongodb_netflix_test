b7359aac6f0f457d9dbdaa4e970e534ff414d818
jaison-leonardo <jlpascuaza@correo.iue.edu.co>
2024-04-05 12:38:32 -0500

    Se actualizo el archivo NeflixDB con pagos y la consulta general + las consultas de eimy

--- NetflixDB.js
+++ NetflixDB.js
@@ -1,4 +1,4 @@
-var path_file = "C:/Users/Jaison-PC/OneDrive - IUE/IUE/2024-1/Base de datos no estructuradas/NetflixDB/"
+var path_file = "C:/Users/Jaison-PC/OneDrive - IUE/IUE/2024-1/Base de datos no estructuradas/NetflixDB/mongodb_netflix_test/"
 load(path_file + "functions.js")
 load(path_file + "data/paises.js")
 load(path_file + "data/idiomas.js")
@@ -60,7 +60,7 @@ var historiales = [
 
 historiales.forEach(function (his) {
     db.usuarios.updateMany(
-        { "_id": "USUARIO_1" },
+        { "_id": "USUARIO_10" },
         { $push: { "perfiles.$[perfil].historial_reproducciones": his } },
         { arrayFilters: [{ "perfil.historial_reproducciones": { $exists: true, $ne: null } }] }
     )
@@ -125,34 +125,34 @@ db.paises.aggregate([
 
 // Recomendaciones
 var usuarios = [
-  {
-    usuario_id: "USUARIO_1",
-    perfil_id: "PERFIL_1"
-  }
+    {
+        usuario_id: "USUARIO_10",
+        perfil_id: "PERFIL_1"
+    }
 ];
 
-usuarios.forEach(function(usuario) {
-  var perfil = db.usuarios.findOne({ "_id": usuario.usuario_id, "perfiles._id": usuario.perfil_id }, { "perfiles.$": 1 });
-  if (perfil && perfil.perfiles.length > 0) {
-    var listaReproduccion = perfil.perfiles[0].historial_reproducciones;
-    var recomendados = [];
+usuarios.forEach(function (usuario) {
+    var perfil = db.usuarios.findOne({ "_id": usuario.usuario_id, "perfiles._id": usuario.perfil_id }, { "perfiles.$": 1 });
+    if (perfil && perfil.perfiles.length > 0) {
+        var listaReproduccion = perfil.perfiles[0].historial_reproducciones;
+        var recomendados = [];
 
-    listaReproduccion.forEach(function(item) {
-      if (item.calificacion_usuario > 3) {
-        recomendados.push({
-          contenido_multimedia: item.contenido_multimedia,
-          razon_recomendacion: "Porque tiene una calificación alta en tu lista de reproducción."
+        listaReproduccion.forEach(function (item) {
+            if (item.calificacion_usuario > 3) {
+                recomendados.push({
+                    contenido_multimedia: item.contenido_multimedia,
+                    razon_recomendacion: "Porque tiene una calificación alta en tu lista de reproducción."
+                });
+            }
         });
-      }
-    });
 
-    if (recomendados.length > 0) {
-      db.usuarios.updateOne(
-        { "_id": usuario.usuario_id, "perfiles._id": usuario.perfil_id },
-        { $set: { "perfiles.$.recomendaciones": recomendados } }
-      );
+        if (recomendados.length > 0) {
+            db.usuarios.updateOne(
+                { "_id": usuario.usuario_id, "perfiles._id": usuario.perfil_id },
+                { $set: { "perfiles.$.recomendaciones": recomendados } }
+            );
+        }
     }
-  }
 });
 
 var userId = "USUARIO_2"
@@ -166,9 +166,197 @@ printjson(result.toArray());
 var usersWithDifferentIPs = detectDifferentIPsInUsers();
 
 // Iterar sobre los resultados y aplicar la función add_excedente_factura
-usersWithDifferentIPs.forEach(function(user) {
+usersWithDifferentIPs.forEach(function (user) {
     var userId = user.usuario._id;
     add_excedente_factura(userId);
 });
 
+// Realizar pago
+var userId = "USUARIO_10"
+var fechaActual = new Date();
+var primerDiaMesActual = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), 1);
+var ultDiaMesActual = new Date(fechaActual.getFullYear(), fechaActual.getMonth() + 1, 0);
+var existePago = db.usuarios.findOne({
+    _id: userId,
+    "preferencias.pago.fecha_pago": {
+        $gte: primerDiaMesActual,
+        $lte: ultDiaMesActual,
+    },
+})
+if (!existePago) {
+    var facturaExistente = db.facturas.findOne({
+        usuario_id: userId,
+        fecha_generacion: {
+            $gte: primerDiaMesActual,
+            $lte: ultDiaMesActual,
+        },
+    })
+    if (!facturaExistente) {
+        id_factura = db.facturas.insertOne({
+            _id: getNextSequenceValue(db.facturas, 'FACTURAS'),
+            usuario_id: userId,
+            costo_base_total: 9.99,
+            excedente_aplicado: false,
+            factura_pagada: false,
+            fecha_generacion: new Date(),
+            fecha_pago: null,
+            saldo_pendiente: 0.0,
+        })
+    }
+    var pago = {
+        metodo_pago: "Tarjeta de credito",
+        monto: 9.99,
+        fecha_pago: new Date(),
+        id_factura: (facturaExistente) ? facturaExistente._id : id_factura.insertedId
+    }
+    db.usuarios.updateMany(
+        { "preferencias.pago": { $type: "string" } },
+        { $set: { "preferencias.pago": [] } }
+    )
+    db.usuarios.updateOne(
+        { _id: userId },
+        { $push: { "preferencias.pago": pago } }
+    )
+    db.facturas.updateOne(
+        { _id: (facturaExistente) ? facturaExistente._id : id_factura.insertedId },
+        {
+            $set: { factura_pagada: true, fecha_pago: new Date() }
+        }
+    )
+} else {
+    print("Ya existe un pago para esa fecha")
+}
 
+db.usuarios.updateOne(
+    { _id: userId },
+    { $unset: { "preferencias.pago": [] } }
+);
+
+
+// Consultar documentos
+db.usuarios.aggregate([
+    {
+        $match: { _id: userId, 'perfiles.nombre_perfil': 'Perfil Principal' }
+    },
+    {
+        $lookup: {
+            from: "contenido_multimedia",
+            localField: "perfiles.historial_reproducciones.contenido_multimedia",
+            foreignField: "_id",
+            as: "contenido_multimedia"
+        }
+    },
+    {
+        $unwind: "$contenido_multimedia"
+    },
+    {
+        $lookup: {
+            from: "paises",
+            localField: "pais",
+            foreignField: "_id",
+            as: "paises_join"
+        }
+    },
+    {
+        $lookup: {
+            from: "generos",
+            localField: "contenido_multimedia.generos",
+            foreignField: "_id",
+            as: "generos_name"
+        }
+    },
+    {
+        $lookup: {
+            from: "restricciones",
+            localField: "contenido_multimedia.restricciones",
+            foreignField: "_id",
+            as: "restricciones_join"
+        }
+    },
+    {
+        $lookup: {
+            from: "actores",
+            localField: "contenido_multimedia.reparto",
+            foreignField: "_id",
+            as: "reparto_join"
+        }
+    },
+    {
+        $lookup: {
+            from: "idiomas",
+            localField: "preferencias.idioma",
+            foreignField: "_id",
+            as: "idiomas_join"
+        }
+    },
+    {
+        $lookup: {
+            from: "facturas",
+            localField: "preferencias.pago.id_factura",
+            foreignField: "_id",
+            as: "facturas_join"
+        }
+    },
+    {
+        $addFields: {
+            "contenido_multimedia.generos": "$generos_name",
+            "contenido_multimedia.restricciones": "$restricciones_join",
+            "contenido_multimedia.reparto": "$reparto_join",
+            "preferencias.idioma": "$idiomas_join",
+            "preferencias.pago": "$facturas_join"
+        }
+    },
+    {
+        $project: {
+            "nombres":1,
+            "apellidos":1,
+            "estado":1,
+            "preferencias": 1,
+            "paises_join.nombre_pais": 1,
+            "contenido_multimedia.nombre_contenido_multimedia": 1,
+            "contenido_multimedia.generos": 1,
+            "contenido_multimedia.restricciones": 1,
+            "contenido_multimedia.reparto": 1
+        }
+    }
+])
+db.usuarios.find()
+db.contenido_multimedia.find()
+db.restricciones.find()
+db.actores.find()
+db.paises.find()
+db.idiomas.find()
+db.facturas.find()
+
+//Actualizar el numero de temporadas
+db.contenido_multimedia.updateOne(
+  { "_id": "ID_CONTENIDO_1", "tipo_contenido.nombre_contenido": "Serie" },
+  { $set: { "tipo_contenido.temporada": 4} } 
+)
+db.contenido_multimedia.find({})
+
+//actualizar la biografia de un actor
+db.actores.updateOne(
+  { "nombre_actor": "Tom Hanks" },
+  { $set: { "biografia": "Muere" } }
+)
+db.actores.find({})
+
+//actualizar la puntuacion de una pelicula o serie
+db.contenido_multimedia.updateOne(
+  { "nombre_contenido_multimedia": "Narcos" }, // Filtrar el contenido multimedia por su nombre
+  { $set: { "puntuacion": NUEVA_PUNTUACION } } // Actualizar la puntuación
+)
+//agregar el campo estado en la coleccion multimedia
+db.contenido_multimedia.updateMany(
+  { }, 
+  { $set: { "estado": "vigente" } }
+)
+
+db.contenido_multimedia.updateMany(
+  { "nombre_contenido_multimedia": { $in: ["La Casa de Papel", "Coco", "La La Land"] } }, 
+  { $set: { "estado": "Próximamente" } } 
+)
+//mostrar que peliculas estan proximas 
+db.contenido_multimedia.find({ "estado": "Próximamente" })
+db.contenido_multimedia.find({})
--- functions.js
+++ functions.js
@@ -1,142 +1,170 @@
 // función que agrega un excedente del 10% a la factura del mes y crea una si no existe
 function add_excedente_factura(id_user) {
-  var fechaActual = new Date();
-  var primerDiaMesActual = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), 1);
-  var ultDiaMesActual = new Date(fechaActual.getFullYear(), fechaActual.getMonth() + 1, 0);
-  var facturaExistente = db.facturas.findOne({
-    usuario_id: id_user,
-    fecha_generacion: {
-      $gte: primerDiaMesActual,
-      $lte: ultDiaMesActual,
-    },
-  });
-  if (facturaExistente && !facturaExistente.excedente_aplicado && !facturaExistente.factura_pagada) {
-    var costo = facturaExistente.costo_base_total;
-    db.facturas.updateOne(
-      { usuario_id: id_user },
-      {
-        $set: {
-          costo_base_total: costo * 1.1,
-          excedente_aplicado: true,
+    var fechaActual = new Date();
+    var primerDiaMesActual = new Date(fechaActual.getFullYear(), fechaActual.getMonth(), 1);
+    var ultDiaMesActual = new Date(fechaActual.getFullYear(), fechaActual.getMonth() + 1, 0);
+    var facturaExistente = db.facturas.findOne({
+        usuario_id: id_user,
+        fecha_generacion: {
+            $gte: primerDiaMesActual,
+            $lte: ultDiaMesActual,
         },
-      }
-    );
-  } else if (facturaExistente && !facturaExistente.excedente_aplicado && facturaExistente.factura_pagada) {
-    var costo = facturaExistente.costo_base_total;
-    db.facturas.updateOne(
-      { usuario_id: id_user },
-      {
-        $set: {
-          saldo_pendiente: costo * 0.1,
-        },
-      }
-    );
-  } else if (!facturaExistente) {
-    db.facturas.insertOne({
-      _id: getNextSequenceValue(db.facturas, 'FACTURAS'),
-      usuario_id: id_user,
-      costo_base_total: 9.99 * 1.1,
-      excedente_aplicado: true,
-      factura_pagada: false,
-      fecha_generacion: new Date(),
-      fecha_pago: null,
-      saldo_pendiente: 0.0,
     });
-  }
+    if (facturaExistente && !facturaExistente.excedente_aplicado && !facturaExistente.factura_pagada) {
+        var costo = facturaExistente.costo_base_total;
+        db.facturas.updateOne(
+            { usuario_id: id_user },
+            {
+                $set: {
+                    costo_base_total: costo * 1.1,
+                    excedente_aplicado: true,
+                },
+            }
+        );
+    } else if (facturaExistente && !facturaExistente.excedente_aplicado && facturaExistente.factura_pagada) {
+        var costo = facturaExistente.costo_base_total;
+        db.facturas.updateOne(
+            { usuario_id: id_user },
+            {
+                $set: {
+                    saldo_pendiente: costo * 0.1,
+                },
+            }
+        );
+    } else if (!facturaExistente) {
+        db.facturas.insertOne({
+            _id: getNextSequenceValue(db.facturas, 'FACTURAS'),
+            usuario_id: id_user,
+            costo_base_total: 9.99 * 1.1,
+            excedente_aplicado: true,
+            factura_pagada: false,
+            fecha_generacion: new Date(),
+            fecha_pago: null,
+            saldo_pendiente: 0.0,
+        });
+    }
 }
+
+
+
 
 // Función obtener un id dependiendo de un documento y un nombre
 function getNextSequenceValue(document, sequenceName) {
-  var sequenceDocument = document.countDocuments() + 1;
-  return sequenceName + '_' + sequenceDocument;
+    var sequenceDocument = document.countDocuments()
+    if (sequenceDocument == 0) {
+        return sequenceName + '_' + sequenceDocument + 1
+    } else {
+        var sequenceValue = db.facturas.aggregate([
+            {
+                $addFields: {
+                    sequenceNumber: {
+                        $toInt: {
+                            $substr: [
+                                "$_id",
+                                { $add: [{ $indexOfBytes: ["$_id", "_"] }, 1] },
+                                -1
+                            ]
+                        }
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    maxSequence: { $max: "$sequenceNumber" }
+                }
+            }
+        ]).toArray()[0].maxSequence
+        return sequenceName + '_' + (parseInt(sequenceValue) + 1)
+    }
 }
 
 function detectDifferentIP(id_usuario) {
-  return db.usuarios.aggregate([
-    // Filtrar por el usuario específico
-    { $match: { _id: id_usuario } },
-    // Desenrollar los perfiles
-    { $unwind: '$perfiles' },
-    // Agrupar por usuario y IP del perfil
-    {
-      $group: {
-        _id: '$_id',
-        usuario: { $first: '$$ROOT' },
-        ips: { $addToSet: '$perfiles.ip_perfil' },
-      },
-    },
-    // Proyectar solo las IP diferentes
-    {
-      $project: {
-        _id: '$_id',
-        usuario: '$usuario',
-        different_ips: {
-          $filter: {
-            input: '$ips',
-            as: 'ip',
-            cond: { $ne: ['$$ip', '$usuario.preferencias.ip'] },
-          },
+    return db.usuarios.aggregate([
+        // Filtrar por el usuario específico
+        { $match: { _id: id_usuario } },
+        // Desenrollar los perfiles
+        { $unwind: '$perfiles' },
+        // Agrupar por usuario y IP del perfil
+        {
+            $group: {
+                _id: '$_id',
+                usuario: { $first: '$$ROOT' },
+                ips: { $addToSet: '$perfiles.ip_perfil' },
+            },
         },
-      },
-    },
-  ]);
+        // Proyectar solo las IP diferentes
+        {
+            $project: {
+                _id: '$_id',
+                usuario: '$usuario',
+                different_ips: {
+                    $filter: {
+                        input: '$ips',
+                        as: 'ip',
+                        cond: { $ne: ['$$ip', '$usuario.preferencias.ip'] },
+                    },
+                },
+            },
+        },
+    ]);
 }
 
 function detectDifferentIPsInUsers() {
-  return db.usuarios.aggregate([
-    // Desenrollar los perfiles
-    { $unwind: '$perfiles' },
-    // Agrupar por usuario y IP del perfil
-    {
-      $group: {
-        _id: '$_id',
-        usuario: { $first: '$$ROOT' },
-        ips: { $addToSet: '$perfiles.ip_perfil' },
-      },
-    },
-    // Proyectar solo las IPs diferentes
-    {
-      $project: {
-        _id: '$_id',
-        usuario: '$usuario',
-        different_ips: {
-          $filter: {
-            input: '$ips',
-            as: 'ip',
-            cond: { $ne: ['$$ip', '$usuario.preferencias.ip'] },
-          },
+    return db.usuarios.aggregate([
+        // Desenrollar los perfiles
+        { $unwind: '$perfiles' },
+        // Agrupar por usuario y IP del perfil
+        {
+            $group: {
+                _id: '$_id',
+                usuario: { $first: '$$ROOT' },
+                ips: { $addToSet: '$perfiles.ip_perfil' },
+            },
         },
-      },
-    },
-    // Filtrar usuarios que tienen IPs diferentes
-    { $match: { different_ips: { $ne: [] } } },
-  ]);
+        // Proyectar solo las IPs diferentes
+        {
+            $project: {
+                _id: '$_id',
+                usuario: '$usuario',
+                different_ips: {
+                    $filter: {
+                        input: '$ips',
+                        as: 'ip',
+                        cond: { $ne: ['$$ip', '$usuario.preferencias.ip'] },
+                    },
+                },
+            },
+        },
+        // Filtrar usuarios que tienen IPs diferentes
+        { $match: { different_ips: { $ne: [] } } },
+    ]);
 }
 
 function detectRestrictedUsers() {
-  return db.usuarios.aggregate([
-    // Desenrollar los perfiles
-    { $unwind: '$perfiles' },
-    // Filtrar perfiles de niños
-    { $match: { 'perfiles.tipo_perfil': 'Niño' } },
-    // Proyectar solo los campos necesarios
-    {
-      $project: {
-        _id: '$_id',
-        nombres: '$nombres',
-        apellidos: '$apellidos',
-        numero_documento: '$numero_documento',
-        perfiles: '$perfiles',
-        restricciones: {
-          $filter: {
-            input: restricciones,
-            as: 'restriccion',
-            cond: { $eq: ['$$restriccion.tipo_restriccion', 'Restricción de Edad - Contenido para Adultos'] },
-          },
+    return db.usuarios.aggregate([
+        // Desenrollar los perfiles
+        { $unwind: '$perfiles' },
+        // Filtrar perfiles de niños
+        { $match: { 'perfiles.tipo_perfil': 'Niño' } },
+        // Proyectar solo los campos necesarios
+        {
+            $project: {
+                _id: '$_id',
+                nombres: '$nombres',
+                apellidos: '$apellidos',
+                numero_documento: '$numero_documento',
+                perfiles: '$perfiles',
+                restricciones: {
+                    $filter: {
+                        input: restricciones,
+                        as: 'restriccion',
+                        cond: { $eq: ['$$restriccion.tipo_restriccion', 'Restricción de Edad - Contenido para Adultos'] },
+                    },
+                },
+            },
         },
-      },
-    },
-    // Filtrar usuarios con restricciones
-    { $match: { restricciones: { $ne: [] } } },
-  ]);
+        // Filtrar usuarios con restricciones
+        { $match: { restricciones: { $ne: [] } } },
+    ]);
 }
